ced4ceacd9a24e69553e7921a0f2563c059d3ffb
juanpablo1978 <jpandrujovich@gmail.com>
2025-03-18 15:44:34 -0300

    ajuste de iconos

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,16 @@ function App() {
  
 
   return (
-    <section className="bg-gray-900 min-h-screen flex justify-center items-center gap-x-4">
-      <FaMarkdown className="text-[140px] text-white"/>
-      <SiTailwindcss className="text-[140px] text-blue-400"/>
-      < FaGitAlt className="text-[140px] text-orange-700"/>
-      <BiLogoTypescript className="text-[140px] text-blue-500"  />
-      <FaReact className="text-[250px] text-blue-400" />
-      < SiNextdotjs className="text-[140px] text-black"/>
-      <FaGithub className="text-[140px] text-white"/>
-      <SiStorybook className="text-[140px] text-pink-500"/>
-      <DiVisualstudio className="text-[140px] text-blue-600"/>
+    <section className="bg-gray-900 min-h-screen flex justify-center items-center gap-x-7">
+      <FaMarkdown className="text-[130px] text-white"/>
+      <SiTailwindcss className="text-[130px] text-blue-400"/>
+      < FaGitAlt className="text-[130px] text-orange-700"/>
+      <BiLogoTypescript className="text-[130px] text-blue-500"  />
+      <FaReact className="text-[230px] text-blue-400" />
+      < SiNextdotjs className="text-[130px] text-black"/>
+      <FaGithub className="text-[130px] text-white"/>
+      <SiStorybook className="text-[130px] text-pink-500"/>
+      <DiVisualstudio className="text-[130px] text-blue-600"/>
     </section>
   )
 }
